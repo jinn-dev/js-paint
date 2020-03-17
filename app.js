bf37c139cf168e7140f4653202a9ca283b241e23
jinn-dev <gsbyyy@naver.com>
2020-03-17 21:24:35 +0900

    Brush Size and Filling Mode

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
-//pixel modifier
-canvas.width = 700;
-canvas.height = 700;
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
-ctx.strokeStyle ="#000000"; //override하고 변경해야 함
+const INITIAL_COLOR = "#000000";
+const CANVAS_SIZE = 700;
+
+//pixel modifier
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR; //override하고 변경해야 함
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function startPainting() {
   painting = true;
@@ -42,16 +50,47 @@ function onMouseDown(event) {
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
+
+function handleRange(event) {
+  const value = event.target.value;
+  ctx.lineWidth = value;
+}
+
+function handleModeClick(event) {
+  if(filling === true) {
+    filling = false;
+    mode.innerText  = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
+function handleCanvasClick(event) {
+  if(filling) {
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
 Array.from(colors).forEach((color) => {
   color.addEventListener("click", handleColorClick);
 });
  //Arrays.from: Object로부터 Array를 만들어준다.
-
 
 if(canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", onMouseDown);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("onMouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
+}
+
+if(range) {
+  range.addEventListener("input", handleRange);
+}
+
+if(mode) {
+  mode.addEventListener("click", handleModeClick);
 }
